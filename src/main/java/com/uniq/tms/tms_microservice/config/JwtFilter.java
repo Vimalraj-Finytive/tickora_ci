9f6d848c7c74a492409ba5373a3634c7647b0b8d
uniqproddev <uniqproddev@gmail.com>
2025-05-07 23:24:13 +0530

    Heroku Privacy policy Angular update

--- src/main/java/com/uniq/tms/tms_microservice/config/JwtFilter.java
+++ src/main/java/com/uniq/tms/tms_microservice/config/JwtFilter.java
@@ -34,7 +34,7 @@ public class JwtFilter implements Filter {
         this.userRepository = userRepository;
     }
 
-    private static final Logger log = LogManager.getLogger(JwtFilter.class);
+    private static final Logger logger = LogManager.getLogger(JwtFilter.class);
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -42,22 +42,22 @@ public class JwtFilter implements Filter {
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
         String jwtToken = null;
-        log.info("fetch user agent from header");
+        logger.info("fetch user agent from header");
         String userAgent = httpRequest.getHeader("User-Agent");
         String ipAddress = jwtUtil.getClientIp(httpRequest);
-        log.info("fetch jwt token from header");
+        logger.info("fetch jwt token from header");
         String authHeader = httpRequest.getHeader("Authorization");
-        System.out.println("JWT_Filter **********userAgent from the token: {}"+ userAgent);
-        System.out.println("JWT_Filter *************IP Address from the token: {}"+ ipAddress);
-        System.out.println("JWT_Filter ************AuthHeader from the token: {}"+ authHeader);
+        logger.debug("JWT_Filter **********userAgent from the token: {}"+ userAgent);
+        logger.debug("JWT_Filter *************IP Address from the token: {}"+ ipAddress);
+        logger.debug("JWT_Filter ************AuthHeader from the token: {}"+ authHeader);
        
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            log.info("jwt token found in header");
+            logger.info("jwt token found in header");
             jwtToken = authHeader.substring(7);
         }
 
         if (jwtToken == null && httpRequest.getCookies() != null) {
-            log.info("fetch jwt token from cookie");
+            logger.info("fetch jwt token from cookie");
             for (Cookie cookie : httpRequest.getCookies()) {
                 if ("JWT_TOKEN".equals(cookie.getName())) {
                     jwtToken = cookie.getValue();
@@ -68,36 +68,36 @@ public class JwtFilter implements Filter {
 
         if (jwtToken != null) {
             try {
-                log.info("parsing jwt token");
+                logger.info("parsing jwt token");
                 Claims claims = jwtUtil.parseToken(jwtToken, userAgent);
                 String email = claims.getSubject();
                 String role = claims.get("roles", String.class);
                 if (email != null && role != null) {
-                    log.info("find user by email from token");
+                    logger.info("find user by email from token");
                     UserEntity user = userRepository.findByEmail(email);
                     if (user == null) {
-                        log.info("user does not exist");
+                        logger.info("user does not exist");
                         httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "User does not exist");
                         return;
                     }
-                    log.info("fetching user role");
+                    logger.info("fetching user role");
                     RoleEntity roleEntity = user.getRole();
                     String userRole = roleEntity.getName();
 
                     if (userRole.startsWith("ROLE_")) {
                         userRole = userRole.substring(5);
                     }
-                    log.info("setting authentication token");
+                    logger.info("setting authentication token");
                     UsernamePasswordAuthenticationToken authentication =
                             new UsernamePasswordAuthenticationToken(email, null,
                                     Collections.singletonList(new SimpleGrantedAuthority(userRole)));
 
                     SecurityContextHolder.getContext().setAuthentication(authentication);
-                    log.info("authentication token set");
+                    logger.info("authentication token set");
                 }
             } catch (Exception e) {
                 e.printStackTrace();
-                log.info("Invalid token",e.getMessage());
+                logger.info("Invalid token",e.getMessage());
                 httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid Token");
                 return;
             }
