2a21852c1c66b2ccdc51821ef02eaf974f9c11ec
Lavanya-Uniq <lavanya.v@uniqtechnologies.co.in>
2026-01-07 13:21:39 +0530

    Version Bug Fix
    1. Apiresponse warning fix for 2 endpoints
    2. dto package importation fix

--- src/main/java/com/uniq/tms/tms_microservice/shared/config/WebConfig.java
+++ src/main/java/com/uniq/tms/tms_microservice/shared/config/WebConfig.java
@@ -11,36 +11,29 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-        // Swagger UI
         registry.addResourceHandler("/swagger-ui/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/springdoc-openapi-ui/");
-
-        // OpenAPI JSON
         registry.addResourceHandler("/v3/api-docs/**")
                 .addResourceLocations("classpath:/META-INF/resources/");
-
-        // Angular static files
         registry.addResourceHandler(
-                        "/",
-                        "/index.html",
-                        "/assets/**",
                         "/*.js",
                         "/*.css",
                         "/*.ico",
-                        "/*.txt"
+                        "/*.txt",
+                        "/*.json"
                 ).addResourceLocations("classpath:/static/browser/")
+                .setCachePeriod(0);
+        registry.addResourceHandler("/assets/**")
+                .addResourceLocations("classpath:/static/browser/assets/")
+                .setCachePeriod(0);
+        registry.addResourceHandler("/", "/index.html")
+                .addResourceLocations("classpath:/static/browser/")
                 .setCachePeriod(0);
     }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-
-        // Forward ONLY non-file, non-api routes
-        registry.addViewController("/{path:[^\\.]*}")
-                .setViewName("forward:/index.html");
-
-        registry.addViewController("/**/{path:[^\\.]*}")
+        registry.addViewController("/{spring:(?!assets|tms|swagger-ui|v3).*}/**")
                 .setViewName("forward:/index.html");
     }
-
 }
